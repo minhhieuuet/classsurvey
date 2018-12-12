e25f33041b9ede702eae5125b778413331643f0b
Hieu Minh Do <minhhieuuet@gmail.com>
2018-12-13 01:16:05 +0700

    404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Error - ClassSurvey</title>
+    <link rel="stylesheet" href="/css/404.css">
+  </head>
+  <body>
+    <h1>Oops!!!! Lỗi rồi ☹</h1>
+    <p class="zoom-area"><b>Đã xảy ra lỗi gì đó hoặc trang bạn đang cố gắng truy cập đã không còn khả dụng. </p>
+    <section class="error-container">
+      <span class="four"><span class="screen-reader-text">4</span></span>
+      <span class="zero"><span class="screen-reader-text">0</span></span>
+      <span class="four"><span class="screen-reader-text">4</span></span>
+    </section>
+    <div class="link-container">
+      <a target="_blank" onclick="window.history.back();" class="more-link">Quay lại trang trước</a>
+    </div>
+  </body>
+</html>
